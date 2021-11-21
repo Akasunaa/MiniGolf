a8b6089e7e89acd105164a1aeed8ee40de039cca
Prométhée <promethee.toneatti@gmail.Com>
2021-11-21 16:16:27 +0100

    putain le multiplicateur fonctionne

--- Assets/Script/GolfBallScript.cs
+++ Assets/Script/GolfBallScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+    using UnityEngine.UI;
+
 
 public class GolfBallScript : MonoBehaviour
 {
@@ -8,69 +10,78 @@ public class GolfBallScript : MonoBehaviour
     private Rigidbody rb;
     private GameObject camera;
     private GameObject floor;
-    [SerializeField] private Vector3 jump = new Vector3(0, 5,0);
+    //public Canvas canva;
+    GameObject multtext;
+    
+    private GameObject start;
+    [SerializeField] private Vector3 jump = new Vector3(0, 5, 0);
+
+    
 
     //jump if is moving
 
     private bool isMoving;
     private bool canJump;
 
+    //force with time push
+
+    private float downTime;
+    private float upTime;
+    private float currentTime;
+    private float startTime = 0f;
+
+    int time;
+
     private void Awake()
     {
         rb = this.GetComponent<Rigidbody>();
-        GameObject start = GameObject.FindWithTag("Respawn");
+        start = GameObject.FindWithTag("Respawn");
+        floor = GameObject.FindGameObjectWithTag("Floor");
+        camera = GameObject.FindGameObjectWithTag("Camera");
         transform.position = start.transform.position;
         isMoving = false;
         canJump = false;
+        //multtext = canva.transform.GetChild(1);
+        multtext = GameObject.FindGameObjectWithTag("Mult");
+        multtext.GetComponent<TMPro.TextMeshProUGUI>().text = "x0";
+        time = 0;
+        
 
     }
 
     private void Update()
     {
-        if (rb.velocity.magnitude >0.1f)
-        {
-            isMoving = true;
-        }
-        else
-        {
-            isMoving = false;
-        }
 
-        camera =GameObject.FindGameObjectWithTag("Camera");
-        floor=GameObject.FindGameObjectWithTag("Floor");
-        if(Input.GetKeyDown("space"))
-        {
-            if (isMoving && canJump)
-            {
-                Jump();
-                print("ismoving and canjump");
-            }
-            else if (!isMoving) {
-                Putting();
-                print("notmoving ");
-            }
-            else {
-                print("ismoving and notjump");
-            }
-            
-        }
+        IsMoving();
+        
+        Timer();
+        TimerInput();
+        PutAndJump();
+
+
+
+
         if (Input.GetKeyDown("r"))
         {
             Die();
         }
-        if(transform.position.y<floor.transform.position.y-20)
+        if (transform.position.y < floor.transform.position.y - 20)
         {
             Die();
         }
-        if(rb.velocity.magnitude < 4)
+        if (rb.velocity.magnitude < 4)
         {
-            rb.velocity=new Vector3(0,0,0);
+            rb.velocity = new Vector3(0, 0, 0);
         }
     }
 
     private void Putting()
     {
-        rb.AddForce(camera.transform.forward.normalized*force);
+        if ((int)currentTime > 5)
+        {
+            currentTime = 5;
+        }
+        rb.AddForce(camera.transform.forward.normalized * force * ((int)currentTime + 1));
     }
 
     public void Die()
@@ -96,5 +107,102 @@ public class GolfBallScript : MonoBehaviour
 
     }
 
+    void IsMoving()
+    {
+        if (rb.velocity.magnitude > 0.1f)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
 
+    void PutAndJump()
+    {
+        if (Input.GetKeyUp("space"))
+        {
+            if (isMoving && canJump)
+            {
+                Jump();
+                print("ismoving and canjump");
+            }
+            else if (!isMoving)
+            {
+                Putting();
+                print("notmoving ");
+            }
+            else
+            {
+                print("ismoving and notjump");
+            }
+
+        }
+    }
+
+    //poubelle
+    void TimerInput2()
+    {
+        float time = Time.time;
+        float timee = 0;
+        while (Input.GetKey("space"))
+        {
+            timee = Time.time - time;
+        }
+
+        print(timee);
+    }
+    //ceci sert à rien mais en fait si parce que je suis nul 
+    void TimerInput()
+    {
+        currentTime = 0;
+        if (Input.GetKeyDown("space"))
+        {
+            downTime = Time.time;
+        }
+        if (Input.GetKeyUp("space"))
+        {
+            upTime = Time.time;
+            currentTime = upTime - downTime;
+        }
+
+    }
+
+    
+    void Timer()
+    {
+        //float startTime = 0;
+        
+
+
+        if (Input.GetKeyDown("space"))
+        {
+            startTime = Time.time;
+            print(startTime);
+            time = 0;
+        }
+
+
+        if (Input.GetKey("space"))
+        {
+            int loop = 0;
+            //startTime = Time.time;
+            while (startTime + time <= Time.time && loop <= 4)
+            {
+                //multtext.text = time.ToString();
+                time += 1;
+                multtext.GetComponent<TMPro.TextMeshProUGUI>().text = ("x" + time.ToString());
+                loop += 1;
+            }
+            time = 0;
+            //multtext.GetComponent<TMPro.TextMeshProUGUI>().text = ("x" + time.ToString());
+        }
+        else if (Input.GetKeyUp("space"))
+        {
+            time = 0;
+            multtext.GetComponent<TMPro.TextMeshProUGUI>().text = ("x" + time.ToString());
+        }
+    }
 }
+
